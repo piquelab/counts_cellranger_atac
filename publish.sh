4d68afcb94f649ef07a7e15deb83a8ce5b89364a
rpique <rpique@gmail.com>
2024-04-26 16:25:37 -0400

    updated publish

--- publish.sh
+++ publish.sh
@@ -1,17 +1,37 @@
+
 #!/bin/bash
 
-destfolder=/nfs/rprscratch/wwwShare/genome.grid.wayne.edu/preeclampsia/${PWD##*/}
+##destfolder=/nfs/rprscratch/wwwShare/genome.grid.wayne.edu/preeclampsia/${PWD##*/}
+prefix=/nfs/rprscratch/wwwShare/cell-ranger-atac/
+
+if [[ -n $(git status --porcelain | grep -v '^??') ]]; then
+    echo "There are uncommitted changes."
+    exit 1
+fi
+
+# Check if the current branch is up to date with its upstream branch
+LOCAL=$(git rev-parse @)
+REMOTE=$(git rev-parse @{u})
+DATE=$(date -Iminutes)
+
+if [ $LOCAL = $REMOTE ]; then
+    echo "Up to date with remote."
+else
+    echo "Diverged from remote."
+    exit 1
+fi
+
+destfolder=${prefix}/${LOCAL}/${DATE}
 
 mkdir -p $destfolder
 
+ln -s $destfolder results
 
 cat libList.txt | while read f; 
 do 
     echo $f 
     cp $f/web_summary.html $destfolder/$f.html 
 done
-
-
 
 
 cp summary.tsv $destfolder/
