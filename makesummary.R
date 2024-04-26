6762f69bca3680deb1f7e8a1794dcec42454037a
rpique <rpique@gmail.com>
2024-04-26 16:07:58 -0400

    Updated makesummary.R

--- makesummary.R
+++ makesummary.R
@@ -1,13 +1,13 @@
-
 library(tidyverse)
 
-system("
-grep 'Est' ./*/metrics_summary.csv | head -1 | sed 's/.*csv:/Library ID,/' > all_summary.csv;
-grep -v 'Est' ./*/metrics_summary.csv | sed 's/\\/metrics_summary.*csv:/,/;s/\\.\\///' >> all_summary.csv
-")
 
+system("
+grep 'Sample' ./*/summary.csv | head -1 | sed 's/.*csv:/Library_ID,/' > all_summary.csv;
+grep -v 'Sample' ./*/summary.csv | sed 's/\\/summary.*csv:/,/;s/\\.\\///' >> all_summary.csv
+")
 
 aa <- read_csv("all_summary.csv")
 
-write_tsv(aa[,c(1:7,18:20,11,16,17)],"summary.tsv")
-print(aa[,c(1:5)])
+write_tsv(aa[,c(2,5,8,9,10,13,14,16,17,19,20,26,28)],"summary.tsv")
+print(aa[,c(2,5,8,9,28)])
+
